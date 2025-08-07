198227901b12b889177b9bbf3ea38402e8c81f6b
fickle <wnsdud987@gmail.com>
2025-08-07 15:19:46 +0900

    프로랭킹 데이터 추가

---
+++ frontend/public/data/proplayerData.ts
@@ -0,0 +1,119 @@
+//proplayerData Data
+
+import avatar1 from "@public/images/users/avatar1.png";
+import avatar2 from "@public/images/users/avatar2.png";
+import avatar3 from "@public/images/users/avatar3.png";
+import avatar4 from "@public/images/users/avatar4.png";
+import avatar5 from "@public/images/users/avatar5.png";
+
+export const proplayerData = [
+  {
+    순위: "01",
+    역할군: "딜러",
+    플레이어: "Proper",
+    주영웅: [avatar1, avatar2, avatar3, avatar4, avatar5],
+    승리: 59,
+    패배: 57,
+    게임횟수: 345,
+    승률: 60,
+  },
+
+  {
+    순위: "02",
+    역할군: "딜러",
+    플레이어: "Flora",
+    주영웅: [avatar1, avatar2, avatar3, avatar4, avatar5],
+    승리: 59,
+    패배: 57,
+    게임횟수: 345,
+    승률: 59,
+  },
+
+  {
+    순위: "03",
+    역할군: "딜러",
+    플레이어: "AlphaYi",
+    주영웅: [avatar1, avatar2, avatar3, avatar4, avatar5],
+    승리: 59,
+    패배: 57,
+    게임횟수: 345,
+    승률: 58,
+  },
+
+  {
+    순위: "04",
+    역할군: "딜러",
+    플레이어: "Pelican",
+    주영웅: [avatar1, avatar2, avatar3, avatar4, avatar5],
+    승리: 59,
+    패배: 57,
+    게임횟수: 345,
+    승률: 57,
+  },
+
+  {
+    순위: "05",
+    역할군: "탱커",
+    플레이어: "Donghak",
+    주영웅: [avatar1, avatar2, avatar3, avatar4, avatar5],
+    승리: 59,
+    패배: 57,
+    게임횟수: 345,
+    승률: 51,
+  },
+
+  {
+    순위: "06",
+    역할군: "딜러",
+    플레이어: "Proud",
+    주영웅: [avatar1, avatar2, avatar3, avatar4, avatar5],
+    승리: 59,
+    패배: 57,
+    게임횟수: 345,
+    승률: 49,
+  },
+
+  {
+    순위: "07",
+    역할군: "탱커",
+    플레이어: "Hanbin",
+    주영웅: [avatar1, avatar2, avatar3, avatar4, avatar5],
+    승리: 59,
+    패배: 57,
+    게임횟수: 345,
+    승률: 47,
+  },
+
+  {
+    순위: "08",
+    역할군: "힐러",
+    플레이어: "Flider",
+    주영웅: [avatar1, avatar2, avatar3, avatar4, avatar5],
+    승리: 59,
+    패배: 57,
+    게임횟수: 345,
+    승률: 45,
+  },
+
+  {
+    순위: "09",
+    역할군: "힐러",
+    플레이어: "FINN",
+    주영웅: [avatar1, avatar2, avatar3, avatar4, avatar5],
+    승리: 59,
+    패배: 57,
+    게임횟수: 345,
+    승률: 43,
+  },
+
+  {
+    순위: "10",
+    역할군: "힐러",
+    플레이어: "Chiyo",
+    주영웅: [avatar1, avatar2, avatar3, avatar4, avatar5],
+    승리: 59,
+    패배: 57,
+    게임횟수: 345,
+    승률: 33,
+  },
+];
