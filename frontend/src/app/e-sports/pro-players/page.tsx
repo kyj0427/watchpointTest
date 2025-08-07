2422990867fa071b3eac85d7a9164aa9fbc768b3
gotohellll <kkimhyelin@gmail.com>
2025-08-07 12:04:51 +0900

    nav상점삭제/e스포츠폴더구조생성중

---
+++ frontend/src/app/e-sports/pro-players/page.tsx
@@ -0,0 +1,25 @@
+import AllTeams from "@/components/sections/teams/AllTeams";
+import Breadcrumb from "@/components/shared/Breadcumb";
+import { headerBannerType, NavLinkProps } from "@/config/types";
+
+const Page = () => {
+    const navLinks: NavLinkProps[] = [
+        { id: 1, url: "/", label: "Home" },
+        { id: 2, url: "", label: "Tournamests" },
+    ];
+    
+
+    const headerData: headerBannerType = {
+        title: "Tournamests",
+        navLinks,
+    };
+
+    return (
+        <main>
+            <Breadcrumb breadcrumb={headerData} />
+            <AllTeams />
+        </main>
+    );
+};
+
+export default Page;
