2cc3a8b7897c73f448b85b5ab19d8c569547a936
hippi <pippikorea@gmail.com>
2025-08-08 11:28:23 +0900

    영웅목록,영웅상세페이지  추가

---
+++ frontend/public/data/heroSkills.ts
@@ -0,0 +1,101 @@
+export const heroSkills = [
+    {
+      id:1,  
+      "name": "생체 소총",
+      "description": "아군은 치유하고 적에게는 피해를 주는 원거리 소총입니다.  누르고 있으면 저격 모드로 전환합니다.",
+      "icon": "https://d15f34w2p8l1cc.cloudfront.net/overwatch/efe0ebb135e87dc26b60f0d20500dcd7553ad121ab2b10cd4ffb5db17be9c977.png",
+      "video": {
+        "thumbnail": "https://images.blz-contentstack.com/v3/assets/blt2477dcaf4ebd440c/bltc56d4d9789a018d0/63390487ed7dcc6a0028039c/ANA_BIOTICRIFLE.jpg",
+        "link": {
+          "mp4": "https://assets.blz-contentstack.com/v3/assets/blt2477dcaf4ebd440c/blt7f91fa726d903e78/6339047ee3c2a2741688cb9f/OVERWATCH_WEBSITE_CHARACTER_CAPTURE_AnaBioticRifle_WEB_16x9_1920x1080p30_H264.mp4",
+          "webm": "https://assets.blz-contentstack.com/v3/assets/blt2477dcaf4ebd440c/blt90138e57c4b91c94/66906cbc689154275a7774e6/OVERWATCH_WEBSITE_CHARACTER_CAPTURE_AnaBioticRifle_WEB_16x9_1920x1080p30_WEBM.webm"
+        }
+      }
+    },
+    {
+      "name": "수면총",
+      "description": "적을 잠에 빠뜨리는 투사체를 발사합니다.",
+      "icon": "https://d15f34w2p8l1cc.cloudfront.net/overwatch/20707fd82265412fdc6d2353daa88ec7558cd71c89aa3ac6cf0e78bbbfcabd80.png",
+      "video": {
+        "thumbnail": "https://images.blz-contentstack.com/v3/assets/blt2477dcaf4ebd440c/bltb27e2783f517c7fe/63390487e3c2a2741688cba3/ANA_SLEEPDART.jpg",
+        "link": {
+          "mp4": "https://assets.blz-contentstack.com/v3/assets/blt2477dcaf4ebd440c/blt35fa57c9d35027cf/6339047eed7dcc6a00280398/OVERWATCH_WEBSITE_CHARACTER_CAPTURE_AnaSleepDart_WEB_16x9_1920x1080p30_H264.mp4",
+          "webm": "https://assets.blz-contentstack.com/v3/assets/blt2477dcaf4ebd440c/bltb6b9ecf5d9a2ec69/66906cca68915405bd7774e8/OVERWATCH_WEBSITE_CHARACTER_CAPTURE_AnaSleepDart_WEB_16x9_1920x1080p30_WEBM.webm"
+        }
+      }
+    },
+    {
+      "name": "생체 수류탄",
+      "description": "수류탄을 던져 아군은 치유하고 치유 효과를 증폭시키며, 적에게는 피해를 주고 치유를 차단합니다.",
+      "icon": "https://d15f34w2p8l1cc.cloudfront.net/overwatch/c8190b234bf0a0e28eecffe162d0c942e6b8656e95f4688c6ca3b025fa5a487d.png",
+      "video": {
+        "thumbnail": "https://images.blz-contentstack.com/v3/assets/blt2477dcaf4ebd440c/bltcd66dcc6eb196187/633904873922a2677fc88cfa/ANA_BIOTICGRENADE.jpg",
+        "link": {
+          "mp4": "https://assets.blz-contentstack.com/v3/assets/blt2477dcaf4ebd440c/blt808b8a165c19d2e6/6339047eb8dbde69f52798e8/OVERWATCH_WEBSITE_CHARACTER_CAPTURE_AnaBioticGrenade_WEB_16x9_1920x1080p30_H264.mp4",
+          "webm": "https://assets.blz-contentstack.com/v3/assets/blt2477dcaf4ebd440c/blt73fcfa06c4272760/66906cd5c5289554ce6104aa/OVERWATCH_WEBSITE_CHARACTER_CAPTURE_AnaBioticGrenade_WEB_16x9_1920x1080p30_WEBM.webm"
+        }
+      }
+    },
+    {
+      "name": "나노 강화제",
+      "description": "아군 한 명의 공격력을 증가시키고 받는 피해를 감소시킵니다.",
+      "icon": "https://d15f34w2p8l1cc.cloudfront.net/overwatch/6fda18b343f3fd0e8dc50fa5a91589e1ca9ed7471a354f61dfc9f22b27b19497.png",
+      "video": {
+        "thumbnail": "https://images.blz-contentstack.com/v3/assets/blt2477dcaf4ebd440c/blt5901c5f790bb82f9/63390487da1d806602f83eee/ANA_NANOBOOST.jpg",
+        "link": {
+          "mp4": "https://assets.blz-contentstack.com/v3/assets/blt2477dcaf4ebd440c/blta81ab58fade9b59b/6339047e8537f87bfbcdc3f8/OVERWATCH_WEBSITE_CHARACTER_CAPTURE_AnaNanoBoost_WEB_16x9_1920x1080p30_H264.mp4",
+          "webm": "https://assets.blz-contentstack.com/v3/assets/blt2477dcaf4ebd440c/blt557e93a659cf572c/66906cdf2cc7b032a7135489/OVERWATCH_WEBSITE_CHARACTER_CAPTURE_AnaNanoBoost_WEB_16x9_1920x1080p30_WEBM.webm"
+        }
+      }
+    }
+];
+
+export const faqItemsTwo = [
+  {
+    id: "1",
+    question: "Things to keep in mind",
+    answer: `
+      When using our services, it’s essential to be well-informed to ensure a smooth experience. 
+      Start by reviewing the terms and conditions associated with the service you are purchasing. 
+      This helps avoid any misunderstandings or unexpected surprises. Communication with the 
+      service provider is crucial; it ensures clarity on details like timelines, expectations, 
+      and deliverables. Additionally, be mindful of deadlines or any time-sensitive aspects of 
+      the service, as delays can sometimes affect the outcome. It’s always a good idea to 
+      double-check all details before proceeding to ensure everything aligns with your needs.
+    `,
+  },
+  {
+    id: "2",
+    question: "Payment Policy",
+    answer: `
+      We require payment to be made in advance to secure any service. This ensures that both 
+      the service provider and client are committed to the agreement. Rest assured that all 
+      payments are processed through a secure payment system to protect your financial 
+      information. Once your payment is complete, you’ll receive a confirmation receipt as 
+      proof of the transaction.
+    `,
+  },
+  {
+    id: "3",
+    question: "Cancellation Policy",
+    answer: `
+      We understand that plans can change, so we offer a flexible cancellation policy. If you 
+      need to cancel a service, it’s best to do so before the service starts or before 
+      significant progress has been made. For cancellations made within the appropriate notice 
+      period, you may be eligible for a full refund. However, cancellations made at the last 
+      minute or after substantial resources have been allocated may incur a cancellation fee.
+    `,
+  },
+  {
+    id: "4",
+    question: "Refund Policy",
+    answer: `
+      Your satisfaction is important to us, which is why we offer a fair and transparent refund 
+      policy. If the service provider is unable to deliver the agreed-upon service or if a 
+      justified cancellation is made, you will be eligible for a refund. Refunds are typically 
+      processed within 5–7 business days after approval, though this timeline may vary slightly 
+      depending on your payment method.
+    `,
+  },
+];
+
---
+++ frontend/src/components/sections/hero/HeroListings.tsx
@@ -0,0 +1,199 @@
+"use client";
+
+import RatingStars from "@/components/ui/RatingStars";
+import { useToggle } from "@/hooks";
+import { Listbox } from "@headlessui/react";
+import { hero } from "@public/data/hero";
+import { IconChevronDown, IconCircleCheckFilled } from "@tabler/icons-react";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import { Autoplay, Navigation, Scrollbar } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+const HeroListings = () => {
+  const filterTypes = ["Popular", "Action", "Adventure", "Sports"];
+
+  const [selectedFilter, setSelectedFilter] = useState(filterTypes[0]);
+
+  const {
+    open: filterOpen,
+    handleToggle: filterToggle,
+    ref: filterRef,
+  } = useToggle();
+
+  return (
+    <section className="section-pt">
+      <div className="container">
+        <div className="flex items-center justify-between flex-wrap gap-24p">
+          <h2 className="heading-2">Marketplace listings</h2>
+          <form className="shrink-0">
+            <Listbox
+              ref={filterRef}
+              value={selectedFilter}
+              onChange={setSelectedFilter}
+              as="div"
+              className="dropdown group"
+            >
+              <Listbox.Button
+                onClick={filterToggle}
+                className="dropdown-toggle toggle-1"
+              >
+                {selectedFilter}
+                <IconChevronDown
+                  className={`${filterOpen && "rotate-180"} icon-24`}
+                />
+              </Listbox.Button>
+              <Listbox.Options className="dropdown-content">
+                {filterTypes.map((item, idx) => (
+                  <Listbox.Option
+                    className={`dropdown-item ${
+                      selectedFilter === item && "active"
+                    }`}
+                    key={idx}
+                    value={item}
+                  >
+                    {item}
+                  </Listbox.Option>
+                ))}
+              </Listbox.Options>
+            </Listbox>
+          </form>
+        </div>
+        <div className="mt-40p">
+          <Swiper
+            loop={true}
+            slidesPerView={4}
+            speed={500}
+            autoplay={{
+              delay: 1000,
+              disableOnInteraction: true,
+              pauseOnMouseEnter: false,
+              reverseDirection: true,
+            }}
+            spaceBetween={24}
+            centeredSlides={false}
+            breakpoints={{
+              0: {
+                slidesPerView: 1,
+                spaceBetween: 14,
+              },
+              576: {
+                slidesPerView: 2,
+                spaceBetween: 16,
+              },
+              768: {
+                slidesPerView: 2,
+                spaceBetween: 16,
+              },
+              1200: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+              },
+              1400: {
+                slidesPerView: 3,
+                spaceBetween: 24,
+              },
+              1600: {
+                slidesPerView: 4,
+                spaceBetween: 30,
+              },
+            }}
+            navigation={{
+              nextEl: ".btn-next",
+              prevEl: ".btn-prev",
+            }}
+            pagination={{
+              clickable: true,
+              el: ".swiper-pagination",
+            }}
+            scrollbar={{
+              el: ".swiper-scrollbar",
+              draggable: true,
+            }}
+            modules={[Autoplay, Navigation, Scrollbar]}
+          >
+            {hero?.map((item, idx) => (
+              <SwiperSlide key={idx} className="swiper-slide">
+                <div
+                  className="relative bg-b-neutral-3 rounded-24 group overflow-hidden w-full h-full "
+                  data-aos="zoom-in"
+                >
+                  <div className="overflow-hidden">
+                    <span className="badge badge-neutral absolute top-3 left-3 z-[2]">
+                      <i className="avatar avatar-primary size-3"></i>
+                      <span className="text-s-regular text-w-neutral-1">
+                        Heroes
+                      </span>
+                    </span>
+                    <Image
+                      src={item?.image}
+                      width={640}
+                      height={310}
+                      className="w-full 3xl:h-[310px] xl:h-[280px] lg:h-[260px] h-[240px] object-cover object-top group-hover:scale-110 group-hover:rotate-2 transition-1"
+                      alt="Playing Valorant"
+                    />
+                  </div>
+                  <div className="p-28p">
+                    <Link
+                      href={`/marketplace/${item?.id}`}
+                      className="heading-3 link-1 mb-2 line-clamp-1"
+                    >
+                      {item?.title}
+                    </Link>
+                    <p className="text-l-regular text-w-neutral-2">
+                      Starting at $ <span className="span">{item?.price}</span>
+                    </p>
+                    <div className="flex-y gap-2 my-24p">
+                      <RatingStars rating={item?.rating} />
+                      <p className="text-base text-w-neutral-1">
+                        {item?.rating} ({item?.reviews})
+                      </p>
+                    </div>
+                    <div className="flex-y flex-wrap gap-3">
+                      <Image
+                        className="size-60p rounded-full shrink-0"
+                        src={item?.author?.image}
+                        width={60}
+                        height={60}
+                        alt="David Liu"
+                      />
+                      <div>
+                        <Link
+                          href="#"
+                          className="flex-y gap-2 text-l-medium link-1 text-w-neutral-1 mb-1"
+                        >
+                          <span>{item?.name}</span>
+                          <IconCircleCheckFilled
+                            size={24}
+                            className="text-secondary"
+                          />
+                        </Link>
+                        <span className="text-s-medium text-w-neutral-3">
+                          {item?.role}
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </SwiperSlide>
+            ))}
+            <div className="flex items-center gap-28p pt-60p">
+              <div className="swiper-navigation swp-navigation-one">
+                <button type="button" className="navigation-btn-one btn-prev">
+                  <i className="ti ti-chevron-left"></i>
+                </button>
+                <button type="button" className="navigation-btn-one btn-next">
+                  <i className="ti ti-chevron-right"></i>
+                </button>
+              </div>
+              <div className="swiper-scrollbar swiper-scrollbar-1"></div>
+            </div>
+          </Swiper>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroListings;
