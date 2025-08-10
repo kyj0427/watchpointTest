9c2b266171a6aead44431f1c79e8c55a8135dd4f
nayeonkim <nykimhei@gmail.com>
2025-08-10 21:47:54 +0900

    전적검색>검색결과>플레이어상세 작업

--- frontend/public/data/userrankListData.ts
+++ frontend/public/data/userrankListData.ts
@@ -1,13 +1,13 @@
 // 주영웅 이미지파일 images>game_hero_hero_portrait 임포트
 import heroPortraits from "@public/images/game_hero/hero_portrait";
-import avatar11 from "@public/images/users/avatar11.png"
-import avatar13 from "@public/images/users/avatar13.png"
+import avatar1 from "@public/images/users/avatar1.png"
 import avatar2 from "@public/images/users/avatar2.png"
+import avatar3 from "@public/images/users/avatar3.png"
 
 export const userrankLists = [{
     목록: "01",
-    아이콘: [avatar11],
-    플레이어: "교회는영어로처치",
+    아이콘: [avatar1],
+    플레이어: "Revolution",
     KDA: "2.15",
     승률: 50,
     플레이시간: 33,
@@ -15,8 +15,8 @@ export const userrankLists = [{
 
     {
     목록: "02",
-    아이콘: [avatar13],
-    플레이어: "교회는영어로처치",
+    아이콘: [avatar2],
+    플레이어: "Revolution",
     KDA: "3.41",
     승률: 40,
     플레이시간: 108,
@@ -24,8 +24,8 @@ export const userrankLists = [{
 
     {
     목록: "03",
-    아이콘: [avatar2],
-    플레이어: "교회는영어로처치",
+    아이콘: [avatar3],
+    플레이어: "Revolution",
     KDA: "1.6",
     승률: 47,
     플레이시간: 304,
