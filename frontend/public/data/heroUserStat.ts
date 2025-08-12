54c01765e713a83a064a00ff1f04ba493ab2c3be
hippi <pippikorea@gmail.com>
2025-08-12 15:50:55 +0900

    영웅별 통계 화면 추가

---
+++ frontend/public/data/heroUserStat.ts
@@ -0,0 +1,518 @@
+export const heroUserStat = [
+  {
+    "key": "ana",
+    "name": "아나",
+    "role": "support",
+    "portrait": "/images/game_hero/hero_portrait/ana.png",
+    "stats": {
+      "pickRate": "7.07%",
+      "winRate": "49.72%",
+      "banRate": "0.54%",
+      "kda": "3.85"
+    }
+  },
+  {
+    "key": "cassidy",
+    "name": "캐서디",
+    "role": "damage",
+    "portrait": "/images/game_hero/hero_portrait/cassidy.png",
+    "stats": {
+      "pickRate": "6.56%",
+      "winRate": "48.86%",
+      "banRate": "0.18%",
+      "kda": "2.53"
+    }
+  },
+  {
+    "key": "kiriko",
+    "name": "키리코",
+    "role": "support",
+    "portrait": "/images/game_hero/hero_portrait/kiriko.png",
+    "stats": {
+      "pickRate": "6.26%",
+      "winRate": "48.2%",
+      "banRate": "0.07%",
+      "kda": "4.01"
+    }
+  },
+  {
+    "key": "genji",
+    "name": "겐지",
+    "role": "damage",
+    "portrait": "/images/game_hero/hero_portrait/genji.png",
+    "stats": {
+      "pickRate": "5.52%",
+      "winRate": "50.3%",
+      "banRate": "0.73%",
+      "kda": "2.36"
+    }
+  },
+  {
+    "key": "soldier76",
+    "name": "솔저: 76",
+    "role": "damage",
+    "portrait": "/images/game_hero/hero_portrait/soldier76.png",
+    "stats": {
+      "pickRate": "4.47%",
+      "winRate": "48.28%",
+      "banRate": "0.04%",
+      "kda": "2.61"
+    }
+  },
+  {
+    "key": "moira",
+    "name": "모이라",
+    "role": "support",
+    "portrait": "/images/game_hero/hero_portrait/moira.png",
+    "stats": {
+      "pickRate": "4.22%",
+      "winRate": "48.07%",
+      "banRate": "0.56%",
+      "kda": "4.87"
+    }
+  },
+  {
+    "key": "sojourn",
+    "name": "소전",
+    "role": "damage",
+    "portrait": "/images/game_hero/hero_portrait/sojourn.png",
+    "stats": {
+      "pickRate": "3.37%",
+      "winRate": "45.43%",
+      "banRate": "0.04%",
+      "kda": "2.5"
+    }
+  },
+  {
+    "key": "orisa",
+    "name": "오리사",
+    "role": "tank",
+    "portrait": "/images/game_hero/hero_portrait/orisa.png",
+    "stats": {
+      "pickRate": "3.22%",
+      "winRate": "48.74%",
+      "banRate": "3.05%",
+      "kda": "3.71"
+    }
+  },
+  {
+    "key": "zenyatta",
+    "name": "젠야타",
+    "role": "support",
+    "portrait": "/images/game_hero/hero_portrait/zenyatta.png",
+    "stats": {
+      "pickRate": "3.19%",
+      "winRate": "48.94%",
+      "banRate": "0.16%",
+      "kda": "4.54"
+    }
+  },
+  {
+    "key": "reaper",
+    "name": "리퍼",
+    "role": "damage",
+    "portrait": "/images/game_hero/hero_portrait/reaper.png",
+    "stats": {
+      "pickRate": "2.94%",
+      "winRate": "51.33%",
+      "banRate": "0.1%",
+      "kda": "2.44"
+    }
+  },
+  {
+    "key": "sigma",
+    "name": "시그마",
+    "role": "tank",
+    "portrait": "/images/game_hero/hero_portrait/sigma.png",
+    "stats": {
+      "pickRate": "2.78%",
+      "winRate": "51.77%",
+      "banRate": "0.17%",
+      "kda": "3.89"
+    }
+  },
+  {
+    "key": "baptiste",
+    "name": "바티스트",
+    "role": "support",
+    "portrait": "/images/game_hero/hero_portrait/baptiste.png",
+    "stats": {
+      "pickRate": "2.57%",
+      "winRate": "48.25%",
+      "banRate": "0.03%",
+      "kda": "3.64"
+    }
+  },
+  {
+    "key": "ramattra",
+    "name": "라마트라",
+    "role": "tank",
+    "portrait": "/images/game_hero/hero_portrait/ramattra.png",
+    "stats": {
+      "pickRate": "2.44%",
+      "winRate": "49.83%",
+      "banRate": "0.19%",
+      "kda": "3.53"
+    }
+  },
+  {
+    "key": "lucio",
+    "name": "루시우",
+    "role": "support",
+    "portrait": "/images/game_hero/hero_portrait/lucio.png",
+    "stats": {
+      "pickRate": "2.4%",
+      "winRate": "52.32%",
+      "banRate": "0.06%",
+      "kda": "3.26"
+    }
+  },
+  {
+    "key": "ashe",
+    "name": "애쉬",
+    "role": "damage",
+    "portrait": "/images/game_hero/hero_portrait/ashe.png",
+    "stats": {
+      "pickRate": "2.38%",
+      "winRate": "51.23%",
+      "banRate": "0.05%",
+      "kda": "2.82"
+    }
+  },
+  {
+    "key": "venture",
+    "name": "벤처",
+    "role": "damage",
+    "portrait": "/images/game_hero/hero_portrait/venture.png",
+    "stats": {
+      "pickRate": "2.38%",
+      "winRate": "51.99%",
+      "banRate": "1.52%",
+      "kda": "2.38"
+    }
+  },
+  {
+    "key": "juno",
+    "name": "주노",
+    "role": "support",
+    "portrait": "/images/game_hero/hero_portrait/juno.png",
+    "stats": {
+      "pickRate": "2.37%",
+      "winRate": "48.99%",
+      "banRate": "0.02%",
+      "kda": "3.8"
+    }
+  },
+  {
+    "key": "tracer",
+    "name": "트레이서",
+    "role": "damage",
+    "portrait": "/images/game_hero/hero_portrait/tracer.png",
+    "stats": {
+      "pickRate": "2.15%",
+      "winRate": "49.87%",
+      "banRate": "1.21%",
+      "kda": "2.29"
+    }
+  },
+  {
+    "key": "dva",
+    "name": "디바",
+    "role": "tank",
+    "portrait": "/images/game_hero/hero_portrait/dva.png",
+    "stats": {
+      "pickRate": "2.14%",
+      "winRate": "49.97%",
+      "banRate": "2.13%",
+      "kda": "5.67"
+    }
+  },
+  {
+    "key": "lifeweaver",
+    "name": "라이프위버",
+    "role": "support",
+    "portrait": "/images/game_hero/hero_portrait/lifeweaver.png",
+    "stats": {
+      "pickRate": "2.13%",
+      "winRate": "48.51%",
+      "banRate": "0.05%",
+      "kda": "4.16"
+    }
+  },
+  {
+    "key": "winston",
+    "name": "윈스턴",
+    "role": "tank",
+    "portrait": "/images/game_hero/hero_portrait/winston.png",
+    "stats": {
+      "pickRate": "2.1%",
+      "winRate": "45.87%",
+      "banRate": "0.3%",
+      "kda": "3.24"
+    }
+  },
+  {
+    "key": "pharah",
+    "name": "파라",
+    "role": "damage",
+    "portrait": "/images/game_hero/hero_portrait/pharah.png",
+    "stats": {
+      "pickRate": "2.02%",
+      "winRate": "48.83%",
+      "banRate": "0.95%",
+      "kda": "2.43"
+    }
+  },
+  {
+    "key": "brigitte",
+    "name": "브리기테",
+    "role": "support",
+    "portrait": "/images/game_hero/hero_portrait/brigitte.png",
+    "stats": {
+      "pickRate": "1.94%",
+      "winRate": "48.98%",
+      "banRate": "0.11%",
+      "kda": "3.62"
+    }
+  },
+  {
+    "key": "mercy",
+    "name": "메르시",
+    "role": "support",
+    "portrait": "/images/game_hero/hero_portrait/mercy.png",
+    "stats": {
+      "pickRate": "1.75%",
+      "winRate": "49.77%",
+      "banRate": "0.13%",
+      "kda": "3.53"
+    }
+  },
+  {
+    "key": "hazard",
+    "name": "해저드",
+    "role": "tank",
+    "portrait": "/images/game_hero/hero_portrait/hazard.png",
+    "stats": {
+      "pickRate": "1.73%",
+      "winRate": "49.01%",
+      "banRate": "0.46%",
+      "kda": "3.53"
+    }
+  },
+  {
+    "key": "bastion",
+    "name": "바스티온",
+    "role": "damage",
+    "portrait": "/images/game_hero/hero_portrait/bastion.png",
+    "stats": {
+      "pickRate": "1.71%",
+      "winRate": "46.04%",
+      "banRate": "0.02%",
+      "kda": "2.64"
+    }
+  },
+  {
+    "key": "illari",
+    "name": "일리아리",
+    "role": "support",
+    "portrait": "/images/game_hero/hero_portrait/illari.png",
+    "stats": {
+      "pickRate": "1.66%",
+      "winRate": "52%",
+      "banRate": "0.04%",
+      "kda": "3.09"
+    }
+  },
+  {
+    "key": "hanzo",
+    "name": "한조",
+    "role": "damage",
+    "portrait": "/images/game_hero/hero_portrait/hanzo.png",
+    "stats": {
+      "pickRate": "1.63%",
+      "winRate": "46.94%",
+      "banRate": "0.04%",
+      "kda": "2.81"
+    }
+  },
+  {
+    "key": "mei",
+    "name": "메이",
+    "role": "damage",
+    "portrait": "/images/game_hero/hero_portrait/mei.png",
+    "stats": {
+      "pickRate": "1.51%",
+      "winRate": "50.33%",
+      "banRate": "0.06%",
+      "kda": "3.05"
+    }
+  },
+  {
+    "key": "widowmaker",
+    "name": "위도우메이커",
+    "role": "damage",
+    "portrait": "/images/game_hero/hero_portrait/widowmaker.png",
+    "stats": {
+      "pickRate": "1.33%",
+      "winRate": "43.5%",
+      "banRate": "0.21%",
+      "kda": "2.13"
+    }
+  },
+  {
+    "key": "junkrat",
+    "name": "정크랫",
+    "role": "damage",
+    "portrait": "/images/game_hero/hero_portrait/junkrat.png",
+    "stats": {
+      "pickRate": "1.3%",
+      "winRate": "47.59%",
+      "banRate": "0.04%",
+      "kda": "2.3"
+    }
+  },
+  {
+    "key": "torbjorn",
+    "name": "토르비욘",
+    "role": "damage",
+    "portrait": "/images/game_hero/hero_portrait/torbjorn.png",
+    "stats": {
+      "pickRate": "1.29%",
+      "winRate": "52.69%",
+      "banRate": "0.06%",
+      "kda": "2.52"
+    }
+  },
+  {
+    "key": "reinhardt",
+    "name": "라인하르트",
+    "role": "tank",
+    "portrait": "/images/game_hero/hero_portrait/reinhardt.png",
+    "stats": {
+      "pickRate": "1.28%",
+      "winRate": "49.66%",
+      "banRate": "0.02%",
+      "kda": "2.47"
+    }
+  },
+  {
+    "key": "symmetra",
+    "name": "시메트라",
+    "role": "damage",
+    "portrait": "/images/game_hero/hero_portrait/symmetra.png",
+    "stats": {
+      "pickRate": "1.24%",
+      "winRate": "54.07%",
+      "banRate": "0.09%",
+      "kda": "2.82"
+    }
+  },
+  {
+    "key": "mauga",
+    "name": "마우가",
+    "role": "tank",
+    "portrait": "/images/game_hero/hero_portrait/mauga.png",
+    "stats": {
+      "pickRate": "1.23%",
+      "winRate": "46.22%",
+      "banRate": "0.06%",
+      "kda": "3.7"
+    }
+  },
+  {
+    "key": "roadhog",
+    "name": "로드호그",
+    "role": "tank",
+    "portrait": "/images/game_hero/hero_portrait/roadhog.png",
+    "stats": {
+      "pickRate": "0.98%",
+      "winRate": "41.81%",
+      "banRate": "0.04%",
+      "kda": "3.64"
+    }
+  },
+  {
+    "key": "junkerqueen",
+    "name": "정커퀸",
+    "role": "tank",
+    "portrait": "/images/game_hero/hero_portrait/junkerqueen.png",
+    "stats": {
+      "pickRate": "0.92%",
+      "winRate": "50.98%",
+      "banRate": "0.02%",
+      "kda": "4.44"
+    }
+  },
+  {
+    "key": "zarya",
+    "name": "자리야",
+    "role": "tank",
+    "portrait": "/images/game_hero/hero_portrait/zarya.png",
+    "stats": {
+      "pickRate": "0.8%",
+      "winRate": "50.54%",
+      "banRate": "20.24%",
+      "kda": "4.07"
+    }
+  },
+  {
+    "key": "echo",
+    "name": "에코",
+    "role": "damage",
+    "portrait": "/images/game_hero/hero_portrait/echo.png",
+    "stats": {
+      "pickRate": "0.73%",
+      "winRate": "46.17%",
+      "banRate": "0.02%",
+      "kda": "2.1"
+    }
+  },
+  {
+    "key": "freja",
+    "name": "프레야",
+    "role": "damage",
+    "portrait": "/images/game_hero/hero_portrait/freja.png",
+    "stats": {
+      "pickRate": "0.72%",
+      "winRate": "48.47%",
+      "banRate": "20.46%",
+      "kda": "2.23"
+    }
+  },
+  {
+    "key": "wreckingball",
+    "name": "레킹볼",
+    "role": "tank",
+    "portrait": "/images/game_hero/hero_portrait/wreckingball.png",
+    "stats": {
+      "pickRate": "0.63%",
+      "winRate": "50.16%",
+      "banRate": "0.62%",
+      "kda": "4.24"
+    }
+  },
+  {
+    "key": "doomfist",
+    "name": "둠피스트",
+    "role": "tank",
+    "portrait": "/images/game_hero/hero_portrait/doomfist.png",
+    "stats": {
+      "pickRate": "0.59%",
+      "winRate": "51.86%",
+      "banRate": "21.87%",
+      "kda": "3.35"
+    }
+  },
+  {
+    "key": "sombra",
+    "name": "솜브라",
+    "role": "damage",
+    "portrait": "/images/game_hero/hero_portrait/sombra.png",
+    "stats": {
+      "pickRate": "0.36%",
+      "winRate": "49.35%",
+      "banRate": "23.16%",
+      "kda": "3.13"
+    }
+  }
+];
